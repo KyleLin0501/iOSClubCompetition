40d4a5ca8bd658faf5dd7f2bc1d9e0c3254de4ba
KyleLin <kylelin930501@gmail.com>
2025-11-18 18:05:18 +0800

    11.18 Commit02

--- iOSClubCompetition/wsgi.py
+++ iOSClubCompetition/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for iOSClubCompetition project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this files, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
